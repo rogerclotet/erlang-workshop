1146fbaccb6dfee5aa2fcabb6f854d7746aecb0c
rogerclotet <clotet89@gmail.com>
2014-05-28 17:04:50 -0700

    Finished star exercise

---
+++ src/star.erl
@@ -0,0 +1,36 @@
+-module(star).
+
+-export([start/2]).
+
+%% Creates a star with N processes connected to him
+%% and pass to each one a message M time.
+start(N, M) -> start_processes(N, N, M).
+
+start_processes(NTotal, 0, _) ->
+    resend_messages(NTotal - 1);
+start_processes(NTotal, N, M) ->
+    Pid = spawn(fun () -> listen() end),
+    Pid ! {message, M, self()},
+    start_processes(NTotal, N - 1, M).
+
+resend_messages(0) -> ok;
+resend_messages(N) ->
+    receive
+        {ack, 0, _} ->
+            resend_messages(N - 1);
+        {ack, M, Pid} ->
+            io:format("Received ack~n"),
+            Pid ! {message, M - 1, self()},
+            resend_messages(N)
+    end.
+
+listen() ->
+    receive
+        {message, M, Pid} ->
+            io:format("Received message (~p missing)~n", [M]),
+            Pid ! {ack, M, self()},
+            if
+                M =:= 0 -> ok;
+                true -> listen()
+            end
+    end.
